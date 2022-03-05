bf3cbd4eb16f7188457f5bb80ae1d2a7d31c36e4
carolinemoore <cmoore365@gmail.com>
2022-03-05 18:24:33 -0500

    Write msgSent status to a text file to persist between script runs.

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import config
 import requests
 from twilio.rest import Client
 from datetime import datetime
+import ast
 
 ef_endpoint = "https://iot1.ecoflow.com/api/v1/devices/queryDeviceData"
 
 device_list = config.ef_devices.items()
 time_now = datetime.now()
 time_now = time_now.strftime("%H:%M")
+
+f = open("data.txt", "r")
+msgSent = f.read()
+msgSent = ast.literal_eval(msgSent)
 
 for device in device_list:
     name = device[0]
@@ -20,8 +25,6 @@ for device in device_list:
 
     response = requests.get(ef_endpoint, params=parameters)
     device_data = response.json()
-
-    msgSent = False
 
     try:
         state_of_charge = device_data["data"]["data"]["socSum"]
@@ -42,7 +45,10 @@ for device in device_list:
 
             print(message.status)
             msgSent = True
-        elif not msgSent and state_of_charge == 100:
+            with open('data.txt', 'w') as f:
+                f.write('True')
+
+        elif not msgSent and state_of_charge >= 99:
             client = Client(config.account_sid, config.auth_token)
 
             message = client.messages.create(
@@ -53,5 +59,12 @@ for device in device_list:
 
             print(message.status)
             msgSent = True
+            with open('data.txt', 'w') as f:
+                f.write('True')
+
         elif 25 < state_of_charge < 100:
             msgSent = False
+            with open('data.txt', 'w') as f:
+                f.write('False')
+
+        print(msgSent)
