77d1dbce7700284624fe39c4686f67cb50328b65
carolinemoore <cmoore365@gmail.com>
2022-03-06 09:22:41 -0500

    Fixed filepath issue when running from command line.

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import requests
 from twilio.rest import Client
 from datetime import datetime
 import ast
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# TODO Figure out how to save the message status for both devices separately
+# TODO Seems to be failing to write correct status to data.txt/write instead to csv file so can have multiple devices and data points?
+# TODO Refactor to use more formulae and Class for Devices?
 
 ef_endpoint = "https://iot1.ecoflow.com/api/v1/devices/queryDeviceData"
 
@@ -10,7 +17,7 @@ device_list = config.ef_devices.items()
 time_now = datetime.now()
 time_now = time_now.strftime("%H:%M")
 
-f = open("data.txt", "r")
+f = open(f"{dir_path}/data.txt", "r")
 msgSent = f.read()
 msgSent = ast.literal_eval(msgSent)
 
@@ -35,29 +42,31 @@ for device in device_list:
     if socFound:
         # noinspection PyUnboundLocalVariable
         if not msgSent and state_of_charge <= 25:
-            client = Client(config.account_sid, config.auth_token)
-
-            message = client.messages.create(
-                body=f"{name} battery level is {state_of_charge}% at {time_now}.",
-                from_=config.from_num,
-                to=config.to_num
-            )
-
-            print(message.status)
+            print(f"{name} battery level is {state_of_charge}% at {time_now}.")
+            # client = Client(config.account_sid, config.auth_token)
+            #
+            # message = client.messages.create(
+            #     body=f"{name} battery level is {state_of_charge}% at {time_now}.",
+            #     from_=config.from_num,
+            #     to=config.to_num
+            # )
+            #
+            # print(message.status)
             msgSent = True
-            with open('data.txt', 'w') as f:
+            with open(f'{dir_path}/data.txt', 'w') as f:
                 f.write('True')
 
         elif not msgSent and state_of_charge >= 99:
-            client = Client(config.account_sid, config.auth_token)
-
-            message = client.messages.create(
-                body=f"{name} battery level is fully charged at {time_now}.",
-                from_=config.from_num,
-                to=config.to_num
-            )
-
-            print(message.status)
+            print(f"{name} battery level is fully charged at {time_now}.",)
+            # client = Client(config.account_sid, config.auth_token)
+            #
+            # message = client.messages.create(
+            #     body=f"{name} battery level is fully charged at {time_now}.",
+            #     from_=config.from_num,
+            #     to=config.to_num
+            # )
+            #
+            # print(message.status)
             msgSent = True
             with open('data.txt', 'w') as f:
                 f.write('True')
@@ -66,5 +75,3 @@ for device in device_list:
             msgSent = False
             with open('data.txt', 'w') as f:
                 f.write('False')
-
-        print(msgSent)
